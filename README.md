3adb67d992492182c0802efd9f7800624d10d030
Jeff Weisman <jweisman11@gmail.com>
2020-08-02 11:07:22 -0400

    broke out supporting functions & persisted data sources

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # lastweektonight
 Analysis of episodes from HBO's Last Week Tonight with John Oliver
+
+### Objective
+* Identify which cuss words are used in episodes of LWT
+* Determine if there is a correlation between episode popularity (US views) and cuss word usage
+
+### Approach
+* Create a listing of common cuss words
+* Scrape episode listing from Wikipedia - https://en.wikipedia.org/wiki/List_of_Last_Week_Tonight_with_John_Oliver_episodes
+* Identify corresponding videos from LWT's YouTube channel (only focusing on "Main Segments")
+* Collect URL and video info from each identified YouTube video
+* Merge the Wikipedia and YouTube data
+* Convert the YouTube video to text using AWS's Transcribe
+* Identify cuss words used in LWT epsidoes
+* Test correlation between cuss words 
+
+### Cloud Dependencies
+* Using GCP's YouTube API which requires creating key
+* Using personal AWS account for transcriptions via Transcribe
+
+### Notes
+* Episode data on Wikipedia includes a count of US viewers, but I'm assuming this is for the original airing on HBO and does not include subsequent views from YouTube. Will be adding this two view counts together to get an accurate estimate of total viewership
+* The first episode from LWT (Season 1, Episode 1) does not apepar on YouTube
+
+### Comparing dates: Wikipedia vs YouTube
+Episodes of HBO's LastWeekTonight air on Sundays. I confirmed all episode's air dates on Wikipedia were Sunday.
+
+The corresponding video for the main segment on YouTube doesn't appear until Monday. LWT also publishes other content to their YouTube channel like web exclusives, special segments like "how is this still a thing" official trailers and extended interviews. I want to exclude this additional content since I'm only focused on main segments.
+
---
+++ lwt_functions.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+
+import requests
+from bs4 import BeautifulSoup
+import lxml
+from googleapiclient.discovery import build
+
+def scrape_wikipedia_tables(url):
+    '''Returns the table html objects from a wikipedia page'''
+    page = requests.get(url)
+
+    # Parse the HTML from URL
+    # From docstirng --> recommended to name a specific parser
+    soup = BeautifulSoup(page.text, 'lxml')
+
+    # Find all tables in HTML
+    all_tables = soup.find_all("table")
+    print(f'# of Tables Found: {len(all_tables)}\n')
+    return all_tables
+
+
+
+# Define function to get statistics from each video
+# From: Python Social Media Analytics, pg. 133
+def get_statistics(video_id, api_key):
+    '''Returns a JSON object with the statistics for a YouTube video'''
+    url = 'https://www.googleapis.com/youtube/v3/videos'
+    pms = {'key':api_key,
+           'id':video_id,
+           'part':'contentDetails,statistics'}
+    res = requests.get(url, pms)
+    data = res.json()
+    return data
+
+
+
+
+# From: https://gist.github.com/Evolution0/6f2a7f36aa27fe3a6ae61b1ec6a37740
+# Define function to paginate through a specific YouTube channel
+def fetch_all_youtube_videos(channel_id, api_key):
+    youtube = build("youtube", "v3", developerKey=api_key)
+    res = youtube.search().list(
+        part="snippet",
+        type="video",
+        channelId=channel_id,
+        maxResults="50"
+    ).execute()
+
+    next_page_token = res['nextPageToken']
+    while 'nextPageToken' in res:
+        next_page = youtube.search().list(
+            part="snippet",
+            type="video",
+            channelId=channel_id,
+            maxResults="50",
+            pageToken=next_page_token
+        ).execute()
+        res['items'] = res['items'] + next_page['items']
+
+        if 'nextPageToken' not in next_page:
+            res.pop('nextPageToken', None)
+        else:
+            next_page_token = next_page['nextPageToken']
+    return res
